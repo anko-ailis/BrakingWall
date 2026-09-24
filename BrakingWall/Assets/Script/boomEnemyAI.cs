2b3cc35e6f00befd20ce33433d6064b890a7ee30
tk.anko.foxer@gmail.com <tk.anko.foxer@gmail.com>
2026-09-24 16:47:50 +0900

    敵が動くようになった

--- BrakingWall/Assets/Script/boomEnemyAI.cs
+++ BrakingWall/Assets/Script/boomEnemyAI.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [SerializeField] GameObject playerObject;   //プレイヤー
+    [SerializeField] float moveSpeed;           //移動速度
+    float playerDistance;                       //プレイヤーとの距離
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //プレイヤーとの距離の計算
+        //playerDistance = Vector3.Distance(this.transform.position, playerObject.transform.position);
+        ////this.transform.LookAt(playerObject.transform);      //プレイヤーの方を向く(3D)
+        Vector3 dir = (playerObject.transform.position - this.transform.position);  //向きの計算
+        this.transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);       // ここで向きたい方向に回転させてます
+        Debug.Log(this.gameObject.name + playerDistance +"" + this.transform.rotation);
+        this.transform.position = Vector3.MoveTowards(this.transform.position, playerObject.transform.position, moveSpeed /100);    //プレイヤー方向に直進
     }
 }

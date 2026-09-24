7eed9d026afae78de0c30e402a60a1daa8d446fc
D:\Users\2512001\ソースツリー <hayato061014@yahoo.co.jp>
2026-09-24 16:50:13 +0900

    プレイヤー操作作りかけ

---
+++ BrakingWall/Assets/script/PlayerMove.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float movespeed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 pos = moveInput.normalized * movespeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + pos);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}                                                                                                                                    
